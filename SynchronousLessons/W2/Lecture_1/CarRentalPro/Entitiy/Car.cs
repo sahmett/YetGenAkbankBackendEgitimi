27bbb608a5ae652305de86ac95b3722accf3cd2b
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-18 21:16:15 +0300

    car oop

---
+++ SynchronousLessons/W2/Lecture_1/CarRentalPro/Entitiy/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalPro.Entitiy
+{
+    public class Car
+    {
+        public Car(int ıd, string brand, string model)
+        {
+            Id = ıd;
+            Brand = brand;
+            Model = model;
+        }
+
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+
+}
