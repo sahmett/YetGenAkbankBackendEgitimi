e6de7f7fecae680756523cf18a319a3076c7ed9e
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-18 17:27:07 +0300

    note

--- AsynchronousLessons/W1.OOPConsole/W1.1.OOPConsole/Program.cs
+++ AsynchronousLessons/W1.OOPConsole/W1.1.OOPConsole/Program.cs
@@ -48,17 +48,45 @@ internal class Program
 
         RAM RAM = new RAM("WD Blue", new Capacity(1, CapacitySizeType.MB));
 
-        Computer cmp4 = new Computer("Apple", "Macbook Air", "Intel i5", RAM,storage);
+        Computer cmp4 = new Computer("Apple", "Macbook Air", "Intel i5", RAM, storage);
         Console.WriteLine($"ID: {cmp4.Id} Marka {cmp4.Brand} Model {cmp4.Model} CPU {cmp4.CPU} RAM {cmp4.RAM.Brand} {cmp4.RAM.Capacity.Size}  {cmp4.Storage.Capacity.Size} {cmp4.Storage.Brand} ");
 
-        /*
-        NOTLAR
+        /*NOTLAR
         Entity nedir?
+        Instance nedir?
         Value Object nedir?
         Enum nedir?
+
+        Entity (Varlık): Genellikle bir veritabanı tablosunu veya veri kaynağındaki özgün bir öğeyi temsil eder.
+        Instance (Örnek): Bir sınıfın belirli bir örneğini veya veriyi temsil eder.
+        Value Object (Değer Nesnesi): Yalnızca değeri taşıyan ve değiştirilemez bir nesnedir, genellikle veri gruplarını temsil eder.
+        Enum (Numaralandırma): Sembolik adlarla sabit değerlerin temsil edildiği bir türdür, sınırlı bir küme değerler içerir.
+
         */
-
-
+        /* Sorular Uzun cevap
+                Entity Nedir?
+        Bir "Entity" (varlık), genellikle bir veritabanı tablosunu veya bir veri kaynağındaki benzersiz bir öğeyi temsil eder.
+        Örneğin, bir e-ticaret uygulamasında, bir "Müşteri" veya "Ürün" bir Entity olabilir. 
+        Entity Framework gibi ORM (Object-Relational Mapping) araçları, bu Entity'leri C# sınıflarıyla eşleştirir ve 
+        veritabanı işlemlerini daha kolay ve nesne odaklı hale getirir.
+        
+        Instance Nedir?
+        Bir "Instance" (örnek), bir sınıfın bir örneğini veya belirli bir veriyi temsil eder. 
+        Bir sınıfın tanımı, bir nesne oluşturulduğunda bu nesnenin özelliklerini ve davranışlarını belirtir. 
+        Örneğin, bir "Araba" sınıfının bir örneği, belirli bir arabanın rengi, modeli, hızı vb. gibi özelliklerini taşır.
+        
+        Value Object Nedir?
+        "Value Object" (değer nesnesi), sadece değerini taşıyan ve özünde değiştirilemez olan bir nesnedir. 
+        Value Object'lar genellikle özellikleri toplu bir şekilde temsil eder ve aynı değere sahip olan
+        Value Object'lar eşdeğer kabul edilir. Örneğin, bir tarih aralığı, coğrafi koordinatlar veya para birimi 
+        gibi veriler Value Object olarak modellenebilir.
+        
+        Enum Nedir?
+        "Enum" (numaralandırma), sabit değerlerin sembolik adlarla temsil edildiği bir türdür.
+        Enum'lar, bir değerin belirli bir sınırlı kümesini temsil etmek için kullanılır.
+        Örneğin, haftanın günleri için bir enum kullanabilirsiniz. Bu, kodun daha anlaşılır ve 
+        yönetilebilir olmasına yardımcı olabilir.
+        */
 
     }
 }
