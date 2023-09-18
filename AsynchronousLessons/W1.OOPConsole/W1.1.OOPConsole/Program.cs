ae40a6040418db74019fd5f05ff70ca0a5610c3c
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-18 13:48:33 +0300

    instance property

--- AsynchronousLessons/W1.OOPConsole/W1.1.OOPConsole/Program.cs
+++ AsynchronousLessons/W1.OOPConsole/W1.1.OOPConsole/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Program_2;
+
+Student st = new Student()
+{
+    Id = 1,
+    FirstName = "Osman",
+    LastName = "Kazancı",
+};
+
+Student st2 = new Student()
+{
+    Id = 2,
+    FirstName = "Aygun",
+    LastName = "Bayram",
+};
+
+
+
+Console.WriteLine($"Name:{st.FirstName} LastName:{st.LastName} ID: {st.Id}");
+Console.WriteLine($"Name:{st2.FirstName} LastName:{st2.LastName} ID: {st2.Id}");
+
+
---
+++ AsynchronousLessons/W1.OOPConsole/W1.1.OOPConsole/Student.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Program_2
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+
+
+
+}
