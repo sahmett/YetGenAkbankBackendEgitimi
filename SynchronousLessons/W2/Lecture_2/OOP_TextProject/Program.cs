7d0eeb6674b07efb313d254caca64731e4b866f9
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-21 21:49:57 +0300

    JsonSerializer

--- SynchronousLessons/W2/Lecture_2/OOP_TextProject/Program.cs
+++ SynchronousLessons/W2/Lecture_2/OOP_TextProject/Program.cs
@@ -1,10 +1,13 @@
 ﻿
 using OOP_TextProject.Entities;
+using System.Text.Json;
 
 var filePath = "C:\\Users\\yelkenliler\\Downloads\\AccessControlLogs.txt";
 var textFile = File.ReadAllText(filePath);
 
 var splitedLines = textFile.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+List<AccessControlLog> logs = new();
 
 foreach(var splitedLine in splitedLines)
 {
@@ -16,7 +19,13 @@ foreach(var splitedLine in splitedLines)
         CreatedOn = DateTimeOffset.Now,
         PersonId = Convert.ToInt64(lineValue[0]),
         DeviceSerialNo = lineValue[1],
-        AccesType = lineValue[2]
+        AccesType = AccessControlLog.ConvertStringToAccesType(lineValue[2]),
+        LogTime = Convert.ToDateTime(lineValue[3]),
     };
+
+    logs.Add(accessControlLog);
 }
 
+Console.WriteLine(JsonSerializer.Serialize(logs));
+File.WriteAllText("C:\\Users\\yelkenliler\\Downloads\\Logs.txt",JsonSerializer.Serialize(logs));
+Console.WriteLine("op. ok");
