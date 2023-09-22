88ef9eec0206396dfaa909afe5ea97cf73d26929
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-22 23:11:27 +0300

    Encapsulation with Get-Set Blocks

--- AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Entities/Accont.cs
+++ AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Entities/Accont.cs
@@ -11,16 +11,33 @@ namespace SpotifyApp.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public int Balance { get; set; }
+        //public int Balance { get; private set; }
+
+        private int balance;
+
+        public int Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (Math.Abs(value - Balance) <= 500)
+                {                      
+                    balance = (value);
+                    Console.WriteLine($"1 Profit - Payment taken, New Balance: {Balance}");
+                }
+                else { Console.WriteLine("1 Profit - Not enough balance, sorry :'( "); }
+            }
+        }
+
 
         public Accont(string name, string surname)
         {
             Name = name;
             Surname = surname;
             Id = Guid.NewGuid();
-            Balance = 1110;
+            balance = 2000;
         }
-
+        #region payment sys
         public void paymentSys(decimal payment)
         {
             if(Balance - payment >= 0) { 
@@ -32,5 +49,6 @@ namespace SpotifyApp.Entities
                 Console.WriteLine("Not enough balance, sorry :'( ");
             }
         }
+        #endregion
     }
 }
--- AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Program.cs
+++ AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Program.cs
@@ -3,4 +3,7 @@ using SpotifyApp.Entities;
 
 Accont a1 = new Accont("ali", "veli");
 
-a1.paymentSys(13);
+//a1.paymentSys(13433);
+a1.Balance -= 400;
+a1.Balance -= 450;
+a1.Balance -= 600;
