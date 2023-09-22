efab5c5eb6bd7174762ba5400fb5b08babad2cad
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-22 23:36:26 +0300

    Class Member and Access Modifiers

---
+++ AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Entities/Playlist.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyApp.Entities
+{
+    public class Playlist
+    {
+        public Guid Id { get; set; }
+        public string Name { get; private set; }
+
+        private List<string> Songs { get; set; }
+
+        public Playlist()
+        {
+            Songs = new List<string>();
+        }
+        public Playlist(string name) : this()
+        { 
+            AddSong(name);
+        }
+
+        public void AddSong(string name)
+        {
+            if(IsSongTitleValid(name))
+                Songs.Add(name);
+        }
+
+        private bool IsSongTitleValid(string name)
+        {
+            return
+                !string.IsNullOrEmpty(name)
+                && name.Length > 2
+                && name.Length < 10;
+        }
+        public void PrintList()
+        {
+            foreach(var song in Songs)
+            {
+                Console.WriteLine(song);
+            }
+        }
+    }
+}
--- AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Program.cs
+++ AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Program.cs
@@ -1,9 +1,18 @@
 ﻿
 using SpotifyApp.Entities;
 
+#region OOP Part
+/*
 Accont a1 = new Accont("ali", "veli");
 
 //a1.paymentSys(13433);
 a1.Balance -= 400;
 a1.Balance -= 450;
 a1.Balance -= 600;
+*/
+#endregion
+
+Playlist leleCano = new Playlist("huma kuşu");
+leleCano.AddSong("babana rahmet");
+leleCano.PrintList();
+
