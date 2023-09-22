91717900dd166ecd69bd512e896485894e1d7e46
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-22 22:05:21 +0300

    guid

---
+++ AsynchronousLessons/W2_OOPAdvance/SpotifyApp/MyEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+new MyEntity();
+
+class MyEntity
+{
+    public string Id { get; set; }
+
+    #region RAND ID
+    /* 
+     public MyEntity()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+            Id += (char)random.Next(48, 90);
+        }
+        Console.WriteLine($"uretilen ID: {Id}");
+    }
+    
+    */
+    #endregion
+    public MyEntity()
+    {
+        Id = Guid.NewGuid();
+        Console.WriteLine(Id);
+    
+    }
+}
--- AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Program.cs
+++ AsynchronousLessons/W2_OOPAdvance/SpotifyApp/Program.cs
@@ -1,24 +1,10 @@
 ﻿
 class program
 {
-static void Main(string[] args)
+
+    static void Main(string[] args)
     {
        
-        
-        Console.WriteLine(idSelect());
     }
 
-    public static string idSelect()
-    {
-        string id=null;
-
-        Random random = new Random();
-       
-        for (int i = 0; i < 20; i++)
-        {
-            id += (char)random.Next(48, 90);
-        }
-        Console.WriteLine($"uretilen ID: {id}");
-        return id;
-    }
 }
