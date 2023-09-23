c66bb9c6fb7f22b5e36a890a603c0a9ac28235f9
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-23 21:09:22 +0300

    interfaces

---
+++ AsynchronousLessons/W2_OOPAdvance/PrinterInterface/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("interfaces");
+SamsungPrinter samsungPrinter = new SamsungPrinter();
+samsungPrinter.PrintPage();
+
+HPPrinter hPPrinter = new HPPrinter();
+hPPrinter.PrintPage();
+
+
+interface IPrinter
+{
+    void PrintPage();
+
+}
+
+class SamsungPrinter : IPrinter
+{
+    public void PrintPage()
+    {
+        Console.WriteLine("B&W");
+    }
+}
+
+class HPPrinter : IPrinter
+{
+    public void PrintPage()
+    {
+        Console.WriteLine("Color");
+    }
+}
