5adffac941ead87fe053804845f9d5276fff3897
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-28 19:39:50 +0300

    constants

--- SynchronousLessons/W3/FreelencerApp/Services/NotepadService.cs
+++ SynchronousLessons/W3/FreelencerApp/Services/NotepadService.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FreelencerApp.Common;
+using FreelencerApp.Constants;
+using FreelencerApp.Entities;
 
 namespace FreelencerApp.Services
 {
