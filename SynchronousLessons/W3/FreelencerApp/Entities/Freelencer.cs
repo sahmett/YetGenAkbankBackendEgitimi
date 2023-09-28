089191cf8470f52381efa25678eae5839a6673a1
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-09-28 19:29:39 +0300

    freelencer

--- SynchronousLessons/W3/FreelencerApp/Entities/Freelencer.cs
+++ SynchronousLessons/W3/FreelencerApp/Entities/Freelencer.cs
@@ -14,11 +14,26 @@ namespace FreelencerApp.Entities
 
         //todo Skills
 
-        public string GetValueCSV()
+        public string GetValuesCVS()
         {
+            return $"{Id},{CreatedOn},{FirstName},{LastName},{WorkExperience}";
+        }
+
+
+        //522af243-34ff-42d8-b98c-16beeb014487,9/25/2023 9:58:57 PM +03:00,John,Doe,5 years of experience in web development
+
+        public void SetValuesCSV(string csvLine)
+        {
+            string[] data = csvLine.Split(',');
+            Id = Guid.Parse(data[0]);
+            CreatedOn = DateTimeOffset.Parse(data[1]); //offset ile farkı ne?
+            FirstName = data[2];
+            LastName = data[3];
+            WorkExperience = data[4];
 
         }
-        
 
+        //todo: Preferred Price
+        //todo: Skills
     }
 }
