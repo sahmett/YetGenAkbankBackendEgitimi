9f0927e5acb3b7c330dc383a8eba0eb0d85bd265
seyyitkilic <seyyit.kilic@beeneo.com>
2023-10-04 11:43:47 +0300

    create obj from txt

--- AsynchronousLessons/W4_AdditionalApplication/LibraryMaster/Program.cs
+++ AsynchronousLessons/W4_AdditionalApplication/LibraryMaster/Program.cs
@@ -2,11 +2,13 @@
 using LibraryMaster.Constants;
 using LibraryMaster.Entity;
 using LibraryMaster.Service;
+using System.IO;
 
 Console.WriteLine("Hello, World!");
 string projectFolder = FileLocations.ProjectFolder;
 
 Console.WriteLine(projectFolder);
+NotepadService notepadService = new NotepadService();
 
 /*
 Console.WriteLine(Directory.GetParent(Directory.GetCurrentDirectory())+"   1!");
@@ -14,6 +16,8 @@ Console.WriteLine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName 
 Console.WriteLine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "   4!");
 Console.WriteLine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "  3!");
 */
+
+/*
 
 // Örnek kullanıcı oluştur
 User user = new User
@@ -41,17 +45,29 @@ BorrowingRequest borrowingRequest = new BorrowingRequest
 };
 
 // Verileri kaydetmek için NotepadService kullan
-NotepadService notepadService = new NotepadService();
+
 notepadService.SaveToNotepad(user);
 notepadService.SaveToNotepad(book);
 notepadService.SaveToNotepad(borrowingRequest);
 
 Console.WriteLine("Veriler başarıyla kaydedildi.");
 
+*/
+
 // Verileri okumak için NotepadService kullan
-string userData = notepadService.GetOnNotepad($"{FileLocations.ProjectFolder}\\Database\\Users.txt");
-string bookData = notepadService.GetOnNotepad($"{FileLocations.ProjectFolder}\\Database\\Books.txt");
-string borrowingRequestData = notepadService.GetOnNotepad($"{FileLocations.ProjectFolder}\\Database\\BorrowingRequests.txt");
+// Path'leri değişken olarak tanımla
+string databaseFolderPath = $"{FileLocations.ProjectFolder}\\Database";
+string userFilePath = $"{databaseFolderPath}\\Users.txt";
+string bookFilePath = $"{databaseFolderPath}\\Books.txt";
+string borrowingRequestFilePath = $"{databaseFolderPath}\\BorrowingRequests.txt";
+
+// Önceki kod buraya gelebilir (veri kaydetme).
+
+// Ardından verileri okumak için NotepadService kullan
+string userData = notepadService.GetOnNotepad(userFilePath);
+string bookData = notepadService.GetOnNotepad(bookFilePath);
+string borrowingRequestData = notepadService.GetOnNotepad(borrowingRequestFilePath);
+
 
 Console.WriteLine("Kullanıcı Verileri:");
 Console.WriteLine(userData);
@@ -64,88 +80,25 @@ Console.WriteLine(borrowingRequestData);
 
 Console.ReadLine();
 
-
-List<User> users = new List<User>
-            {
-                new User
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "John Doe",
-                    UserDetail = "User 1 details here"
-                },
-                new User
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Jane Smith",
-                    UserDetail = "User 2 details here"
-                }
-            };
-
-// Örnek kitaplar oluştur
-List<Book> books = new List<Book>
-            {
-                new Book
-                {
-                    Id = Guid.NewGuid(),
-                    Writer = "Author A",
-                    Title = "Book 1"
-                },
-                new Book
-                {
-                    Id = Guid.NewGuid(),
-                    Writer = "Author B",
-                    Title = "Book 2"
-                }
-            };
-
-// Örnek ödünç alma istekleri oluştur
-List<BorrowingRequest> borrowingRequests = new List<BorrowingRequest>
-            {
-                new BorrowingRequest
-                {
-                    Id = Guid.NewGuid(),
-                    BorrowingBy = "User 1",
-                    BorrowStartDate = DateTime.Now,
-                    BorrowEndDate = DateTime.Now.AddDays(14)
-                },
-                new BorrowingRequest
-                {
-                    Id = Guid.NewGuid(),
-                    BorrowingBy = "User 2",
-                    BorrowStartDate = DateTime.Now,
-                    BorrowEndDate = DateTime.Now.AddDays(7)
-                }
-            };
-
-// Verileri kaydetmek için NotepadService kullan
-
-foreach (var user2 in users)
+string[] lines = bookData.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+List<Book> bookList = new List<Book>();
+foreach (string line in lines)
 {
-    notepadService.SaveToNotepad(user2);
+    string[] parts = line.Split(',');
+
+    if (parts.Length == 2)
+    {
+        Book book = new Book();
+        book.Writer = parts[0].Trim();
+        book.Title = parts[1].Trim();
+        bookList.Add(book);
+    }
 }
 
-foreach (var book2 in books)
+foreach (Book book in bookList)
 {
-    notepadService.SaveToNotepad(book2);
+    Console.WriteLine($"Writer: {book.Writer}");
+    Console.WriteLine($"Title: {book.Title}");
+    Console.WriteLine(); // Boş satır eklemek için
 }
-
-foreach (var request in borrowingRequests)
-{
-    notepadService.SaveToNotepad(request);
-}
-
-Console.WriteLine("Veriler başarıyla kaydedildi.");
-
-Console.WriteLine("Kullanıcı Verileri:");
-Console.WriteLine(userData);
-
-Console.WriteLine("Kitap Verileri:");
-Console.WriteLine(bookData);
-
-Console.WriteLine("Ödünç Alma İsteği Verileri:");
-Console.WriteLine(borrowingRequestData);
-
 Console.ReadLine();
-
-
-
