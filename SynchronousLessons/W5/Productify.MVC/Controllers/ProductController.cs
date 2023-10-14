fcc2ac3cdff17766f3bdf7aa16103ba59e8e539f
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-10-14 13:44:13 +0300

    git

---
+++ SynchronousLessons/W5/Productify.MVC/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Productify.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
