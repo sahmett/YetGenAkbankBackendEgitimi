26f966ef5706d07663b367f40cad203e7ac11b8d
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-10-14 13:57:40 +0300

    linq where

--- AsynchronousLessons/W4_AdditionalApplication/LinqApp/Program.cs
+++ AsynchronousLessons/W4_AdditionalApplication/LinqApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Program.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
 Console.WriteLine("Hello, World!");
+
+
+int[] sayilar = new int[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+Console.WriteLine("\n kucukSayilar1 ");
+
+var kucukSayilar1 = from sayi in sayilar
+                    where sayi < 5
+                    select sayi;
+
+foreach (int sayi in kucukSayilar1)
+{
+    Console.Write($"{sayi},");
+}
+
+Console.WriteLine("\n kucukSayilar2 ");
+
+
+var kucukSayilar2 = sayilar.Where(x => x < 5);
+
+foreach (int sayi in kucukSayilar2)
+{
+    Console.Write($"{sayi},");
+}
