75b9d39b677bf2844da9d735efec595a0cba725d
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-10-24 15:00:07 +0300

    [Feature]: User addres value obj. added

--- SynchronousLessons/W7/RentCar/RentCar.Domain/Entities/User.cs
+++ SynchronousLessons/W7/RentCar/RentCar.Domain/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,6 @@ namespace RentCar.Domain.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public Address? Address { get; set; }
     }
 }
