a255ccdf3515b3858ae3545858783fe0069918cb
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-10-24 10:59:20 +0300

    [Feature]: Added MVC and cars page

---
+++ SynchronousLessons/W6/CarMatik/CarMatik.MVCClient/Controllers/CarsController.cs
@@ -0,0 +1,22 @@
+﻿using CarMatik.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarMatik.MVCClient.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CarsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cars = await _httpClient.GetFromJsonAsync<List<Car>>("Cars");
+
+            return View(cars);
+        }
+    }
+}
