602bbea8d9a73f33e2b0c189590a643292ac4698
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-11-07 16:43:06 +0300

    [added]: job hunter domain class lib added

---
+++ AsynchronousLessons/W6/JobHunter/JobHunter.Domain/Entities/JobPost.cs
@@ -0,0 +1,24 @@
+﻿using JobHunter.Domain.Common;
+using JobHunter.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHunter.Domain.Entities
+{
+    public class JobPost : EntityBase<Guid>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Company {  get; set; }
+        public WorkMode WorkMode { get; set; }
+
+
+        public JobPost() 
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
