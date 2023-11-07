95ddb3cdbea88a52a1e44c181c04cb509894b69e
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-11-08 00:33:04 +0300

    [Added]: Migration 2 added

--- AsynchronousLessons/W6/JobHunter/JobHunter.Persistence/Migrations/20231107160158_mig_2_update_JobPostsTable.Designer.cs
+++ AsynchronousLessons/W6/JobHunter/JobHunter.Persistence/Migrations/20231107160158_mig_2_update_JobPostsTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JobHunter.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace JobHunter.Persistence.Migrations
 {
     [DbContext(typeof(JobHunterDbContext))]
-    partial class JobHunterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231107160158_mig_2_update_JobPostsTable")]
+    partial class mig_2_update_JobPostsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AsynchronousLessons/W6/JobHunter/JobHunter.Persistence/Migrations/20231107160158_mig_2_update_JobPostsTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobHunter.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2_update_JobPostsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "JobPosts",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "JobPosts");
+        }
+    }
+}
