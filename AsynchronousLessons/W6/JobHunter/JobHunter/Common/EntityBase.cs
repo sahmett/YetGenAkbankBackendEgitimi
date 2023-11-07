fc2b04bf6c206a6373b40e770e977e96efe73f98
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-11-07 16:44:46 +0300

    [added]: job hunter domain class entity base, job post and work mode class added

---
+++ AsynchronousLessons/W6/JobHunter/JobHunter/Common/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHunter.Common
+{
+	public class EntityBase<Tkey>
+	{
+		public Tkey Id { get; set; }
+		public DateTime CreatedOn { get; set; }
+	}
+}
