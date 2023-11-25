3ce79143e85bfde7dfd967df53878a22e5903c7f
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-11-25 13:05:16 +0300

    [Added] : yetgenakbankjump project applicationDbCOntextFactory.cs added

---
+++ YetgenAkbankJump/YetgenAkbankJump.Persistence/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace YetgenAkbankJump.Persistence.Contexts
+{
+	public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+	{
+		public ApplicationDbContext CreateDbContext(string[] args)
+		{
+			// appsettings.json dosyasının konumu
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "..", "YetgenAkbankJump.WebApi");
+
+			IConfigurationRoot configuration = new ConfigurationBuilder()
+				.SetBasePath(path)
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+
+			var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+			var connectionString = configuration.GetSection("YetgenPostgreSQLDB").Value;
+
+			optionsBuilder.UseNpgsql(connectionString);
+
+			return new ApplicationDbContext(optionsBuilder.Options);
+		}
+	}
+}
