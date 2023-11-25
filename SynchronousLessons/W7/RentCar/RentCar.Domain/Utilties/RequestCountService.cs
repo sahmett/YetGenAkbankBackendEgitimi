69968b3698efb0d749601607af7e0a67dad9d2d5
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-11-26 01:11:01 +0300

    [Added] RentCar.Domain RequestCountService.cs

---
+++ SynchronousLessons/W7/RentCar/RentCar.Domain/Utilties/RequestCountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Domain.Utilties
+{
+	public class RequestCountService
+	{
+		public int Count { get; set; } = 0;
+	}
+}
