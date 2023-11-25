13dc2f966a2c4aafe25dcdf70ee8fd4e71de36aa
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-11-25 13:04:21 +0300

    [Added] : yetgenakbankjump project context class "application db context cs " added

---
+++ YetgenAkbankJump/YetgenAkbankJump.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using YetgenAkbankJump.Domain.Entities;
+
+namespace YetgenAkbankJump.Persistence.Contexts
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<Student> Students { get; set; }
+			
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions):base(dbContextOptions)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly
+				.GetExecutingAssembly());
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
