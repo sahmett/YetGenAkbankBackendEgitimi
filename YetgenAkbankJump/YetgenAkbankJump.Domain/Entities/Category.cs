12afd1d69820785e7973e04950afec88ceb1b5db
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-04 17:17:39 +0300

    [Added] : Domain category.cs and product.cs

---
+++ YetgenAkbankJump/YetgenAkbankJump.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetgenAkbankJump.Domain.Entities
+{
+	public class Category : EntityBase<Guid>
+	{
+        public string Name { get; set; }
+		public ICollection<Product> Products { get; set; }
+
+	}
+}
---
+++ YetgenAkbankJump/YetgenAkbankJump.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetgenAkbankJump.Domain.Entities
+{
+	public class Product : EntityBase<Guid>
+	{
+        public string Name { get; set; }
+		public Guid CategoryId { get; set; }
+		public Category Category { get; set; }
+	}
+}
