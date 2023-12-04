7b9de3a22694b697e30495c683b6f2847cda3a0f
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-04 17:19:19 +0300

    [Added] : category and product configuration.cs docs

---
+++ YetgenAkbankJump/YetgenAkbankJump.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YetgenAkbankJump.Domain.Entities;
+
+namespace YetgenAkbankJump.Persistence.Configurations
+{
+	public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+	{
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			// ID 
+			builder.HasKey(x => x.Id);
+			builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+			// Name
+			builder.Property(x => x.Name).IsRequired();
+			builder.Property(x => x.Name).HasMaxLength(75);
+
+		// Common Fields
+
+			// CreatedByUserId
+			builder.Property(x => x.CreatedByUserId).IsRequired();
+			builder.Property(x => x.CreatedByUserId).HasMaxLength(75);
+
+			// CreatedOn
+			builder.Property(x => x.CreatedOn).IsRequired();
+
+			// ModifiedByUserId
+			builder.Property(x => x.ModifiedByUserId).IsRequired(false);
+			builder.Property(x => x.ModifiedByUserId).HasMaxLength(75);
+
+			// LastModifiedOn
+			builder.Property(x => x.LastModifiedOn).IsRequired(false);
+
+			// DeletedByUserId
+			builder.Property(x => x.DeletedByUserId).IsRequired(false);
+			builder.Property(x => x.DeletedByUserId).HasMaxLength(75);
+
+			// DeletedOn
+			builder.Property(x => x.DeletedOn).IsRequired(false);
+
+			// IsDeleted
+			builder.Property(x => x.IsDeleted).IsRequired();
+
+			//	Relationships
+			//Ör-> Categoryden >> bu kategorinin çok productu var, productun 1 kategorisi var
+			//Ör-> Producttan  >> bu productun bir kategorisi var, kategorinin çok productu var
+
+			builder.HasMany(x => x.Products)
+				.WithOne(x => x.Category)
+				.HasForeignKey(x => x.CategoryId);
+
+			// Category ID
+
+			// Category 
+
+			builder.ToTable("Categories");
+		}
+
+	}
+
+}
