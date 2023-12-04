804ece5af319377b8e25af7750abbdc4c74c82fd
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-04 17:20:08 +0300

    [Added] : MVC project added

---
+++ YetgenAkbankJump/YetgenAkbankJump.MVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resend;
+using System.Diagnostics;
+using YetgenAkbankJump.MVC.Models;
+
+namespace YetgenAkbankJump.MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IResend _resend;
+		public HomeController(ILogger<HomeController> logger, IResend resend)
+		{
+			_logger = logger;
+			_resend = resend;
+		}
+
+		public async Task<IActionResult> IndexAsync()
+		{
+			var message = new EmailMessage();
+
+			message.From = "onboarding@resend.dev";
+			message.To.Add("myapp@example.com");
+			message.Subject = "Hello!";
+			message.HtmlBody = "<div><strong>Greetings<strong> 👋🏻 from .NET</div>";
+
+			await _resend.EmailSendAsync(message);
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
