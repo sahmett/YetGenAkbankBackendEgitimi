64c23b7ba8f514902f04d4395363985f6f5338e6
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-05 18:14:00 +0300

    [Added] : MyAppInfrastructure MyappDbContext.cs added

---
+++ SynchronousLessons/W12/MyApp/MyApp.Infrastructure/Contexts/MyAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Infrastructure.Contexts
+{
+	public class MyAppDbContext : DbContext
+	{
+		public DbSet<Person> DefraudedPeople { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseInMemoryDatabase("MyAppDb");
+		}
+	}
+}
