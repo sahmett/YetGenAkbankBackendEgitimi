a142c7a88c32f6659142e0cbeb54478c26abf5bf
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-06 22:14:33 +0300

    [Created] : IdentityMVC project added

---
+++ YetgenAkbankJump/YetgenAkbankJump.IdentityMVC/ViewModel/AuthLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YetgenAkbankJump.IdentityMVC.ViewModel
+{
+	public class AuthLoginViewModel
+	{
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Password { get; set; }
+
+	}
+}
