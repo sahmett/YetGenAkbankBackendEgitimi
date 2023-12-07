62e242ffd6fe2d484f2d2058a36986e8da0d102a
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-07 22:54:40 +0300

    [Added] more linq examples added

--- AsynchronousLessons/W4_AdditionalApplication/LinqApp/Program.cs
+++ AsynchronousLessons/W4_AdditionalApplication/LinqApp/Program.cs
@@ -34,3 +34,41 @@ foreach (int sayi in kucukSayilar2)
     Console.Write($"{sayi},");
 }
 Console.Write("");
+
+
+// Example 1: Select
+Console.WriteLine("\nExample 1: Select");
+
+var squaredNumbers = sayilar.Select(x => x * x);
+
+foreach (int squaredNumber in squaredNumbers)
+{
+    Console.Write($"{squaredNumber},");
+}
+Console.WriteLine();
+
+
+//More Examples:
+
+// Example 2: OrderBy
+Console.WriteLine("\nExample 2: OrderBy");
+
+var orderedNumbers = sayilar.OrderBy(x => x);
+
+foreach (int number in orderedNumbers)
+{
+    Console.Write($"{number},");
+}
+Console.WriteLine();
+
+// Example 3: Any
+Console.WriteLine("\nExample 3: Any");
+
+bool hasEvenNumber = sayilar.Any(x => x % 2 == 0);
+Console.WriteLine($"Has even number: {hasEvenNumber}");
+
+// Example 4: Sum
+Console.WriteLine("\nExample 4: Sum");
+
+int sumOfNumbers = sayilar.Sum();
+Console.WriteLine($"Sum of numbers: {sumOfNumbers}");
